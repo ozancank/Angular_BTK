18eac02ff1fc00bdd015bdd447234b476ca7a268
ozancank <ozancank@gmail.com>
2020-10-25 23:51:56 +0300

    First Backend

---
+++ SehirRehberi.API/SehirRehberi.API/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SehirRehberi.API.Data;
+using System.Threading.Tasks;
+
+namespace SehirRehberi.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(v => v.Id == id);
+            return Ok(value);
+        }
+    }
+}
